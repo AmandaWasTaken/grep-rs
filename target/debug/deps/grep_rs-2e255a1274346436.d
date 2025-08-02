e8e736d44f48c23455eec2fdef151979faa087c3
AmandaWasTaken <amandasandell02@gmail.com>
2025-08-02 18:05:31 +0300

    add gitignore and ignore target/

--- target/debug/deps/grep_rs-2e255a1274346436.d
+++
@@ -1,5 +0,0 @@
-/home/amanda/Coding/grep-rs/target/debug/deps/grep_rs-2e255a1274346436.d: src/main.rs
-
-/home/amanda/Coding/grep-rs/target/debug/deps/grep_rs-2e255a1274346436: src/main.rs
-
-src/main.rs:
--- target/debug/grep-rs.d
+++
@@ -1,1 +0,0 @@
-/home/amanda/Coding/grep-rs/target/debug/grep-rs: /home/amanda/Coding/grep-rs/src/main.rs
